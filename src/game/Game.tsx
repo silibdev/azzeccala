a0c6db23683c5723713e3acfff4560e39ec94b43
Fabio Siliberto <fabio.siliberto@gmail.com>
2022-06-19 17:12:04 +0200

    Refactoring keyboard

--- src/game/Game.tsx
+++ src/game/Game.tsx
@@ -1,11 +1,11 @@
 import { Keyboard } from './components/Keyboard';
 import { WordGuesses } from './components/WordGuesses';
 import {
-  checkExpiredWord,
+  checkExpiredWord, checkWord,
   DEFAULT_GAME_STATE,
   GameContext,
-  GameState,
-  LetterStateEnum
+  GameState, LetterGuess,
+  LetterStateEnum, WordGuess
 } from '../contexes/GameContext';
 import { Dispatch, SetStateAction, useContext, useEffect, useState } from 'react';
 import { GameOver } from './components/GameOver';
@@ -41,18 +41,92 @@ export const Game = () => {
   }, []);
 
   const currentIndex = gameState.guesses.length - 1;
-  const currentGuesses = gameState.guesses[currentIndex];
+  const currentWordGuess: WordGuess | undefined = gameState.guesses[currentIndex];
+  const currentLetters = currentWordGuess.letters;
 
-  const lose = currentIndex === 5 && currentGuesses && currentGuesses.letters.length && currentGuesses.letters[0]?.state !== LetterStateEnum.EMPTY;
-  const win = currentGuesses && !!currentGuesses.letters.length && currentGuesses.letters.every(lg => lg.state === LetterStateEnum.CORRECT);
+  const lose = currentIndex === 5 && currentWordGuess?.letters[0]?.state !== LetterStateEnum.EMPTY;
+  const win = currentWordGuess?.letters.every(lg => lg.state === LetterStateEnum.CORRECT);
   const finish = win || lose;
+
+  const usedLetters: Record<string, LetterStateEnum> = gameState.guesses
+    .map(wg => wg.letters)
+    .flat()
+    .reduce<Record<string, LetterStateEnum>>((letterMap, lg) => {
+      if (!letterMap[lg.letter] || lg.state === LetterStateEnum.CORRECT) {
+        letterMap[lg.letter] = lg.state;
+      }
+      return letterMap;
+    }, {});
+
+  const updateGameState = (letters: LetterGuess[], id?: number, word?: string) => {
+    gameState.guesses[currentIndex] = {
+      ...currentWordGuess,
+      letters
+    };
+    if (typeof id === 'number') {
+      gameState.id = id;
+    }
+    if (typeof word === 'string') {
+      gameState.word = word;
+    }
+    setGameState({...gameState});
+  }
+
+  const confirmWord = async () => {
+    if (currentLetters.length < 5) {
+      return;
+    }
+    setLoader(true);
+    const [{letters, id, word}, expired] = await Promise.all([
+      checkWord(currentLetters),
+      checkExpiredWord(gameState.id)
+    ])
+    setLoader(false);
+    if (expired.isExpired || gameState.id !== id) {
+      setGameState({
+        ...DEFAULT_GAME_STATE(),
+        id: expired.id
+      })
+    } else {
+      updateGameState(letters, id, word);
+    }
+  }
+
+  const deleteLastLetter = () => {
+    currentLetters.pop();
+    updateGameState(currentLetters);
+  }
+
+  const addLetter = (letter: string) => () => {
+    if (currentLetters.length >= 5) {
+      return;
+    }
+
+    updateGameState(currentLetters.concat({
+      letter,
+      state: LetterStateEnum.EMPTY
+    }))
+  }
+
+  const buttonClicked = async (button: string | 'Enter' | 'Delete') => {
+    switch (button) {
+      case 'Enter':
+        await confirmWord();
+        break;
+      case 'Delete':
+        deleteLastLetter()
+        break;
+      default:
+        addLetter(button);
+    }
+  }
 
   return (
     <div>
       <GameContext.Provider value={gameStateArr}>
         <WordGuesses wordGuesses={gameState.guesses}></WordGuesses>
         {!finish
-          ? <Keyboard></Keyboard>
+          ? <Keyboard usedLetters={usedLetters} buttonClicked={buttonClicked}></Keyboard>
           : <GameOver gameState={gameState} win={win}></GameOver>}
       </GameContext.Provider>
     </div>
